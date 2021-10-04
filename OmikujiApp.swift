c9d8fe64dc240c1bdaa3fb5dc3c5f749ec5e892a
Shikaruuuu <40463368+Shikaruuuu@users.noreply.github.com>
2021-10-05 01:21:31 +0900

    Omikuji App first

---
+++ OmikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiApp.swift
+//  Omikuji
+//
+//  Created by 横森暉 on 2021/09/11.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
